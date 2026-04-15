82668ee8625afd69e2bbdc77ea895b8e33ac5c98
Herve Desrosiers <hdesrosiers@stingray.com>
2026-04-15 07:53:21 -0400

    pass explicit element prop instead of implicit children prop

--- src/App.jsx
+++ src/App.jsx
@@ -62,13 +62,22 @@ const App = () => {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <Box>
+        <Box element={<MovieList movies={movies} />} />
+        <Box
+          element={
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMovieList watched={watched} />
+            </>
+          }
+        />
+        {/* <Box>
           <MovieList movies={movies} />
         </Box>
         <Box>
           <WatchedSummary watched={watched} />
           <WatchedMovieList watched={watched} />
-        </Box>
+        </Box> */}
       </Main>
     </>
   );
@@ -118,7 +127,7 @@ const Main = ({ children }) => {
   return <main className="main">{children}</main>;
 };
 
-const Box = ({ children }) => {
+const Box = ({ element }) => {
   const [isOpen, setIsOpen] = useState(true);
 
   return (
@@ -126,7 +135,7 @@ const Box = ({ children }) => {
       <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
         {isOpen ? "–" : "+"}
       </button>
-      {isOpen && children}
+      {isOpen && element}
     </div>
   );
 };
