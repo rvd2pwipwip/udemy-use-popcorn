d0f51f0205e262ea7f6074188f0741a257c96187
Herve Desrosiers <hdesrosiers@stingray.com>
2026-04-22 08:21:51 -0400

    reusability with props @ https://www.udemy.com/course/the-ultimate-react-course/learn/lecture/37350750#overview

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -12,41 +12,63 @@ const StarContainerStyle = {
   gap: "0.25rem",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-
-const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = React.useState(0);
+const StarRating = ({
+  maxRating = 5,
+  color = "#fcc419",
+  size = 3,
+  className = "",
+  messages = [],
+  defaultRating = 0,
+  onSetRating,
+}) => {
+  const [rating, setRating] = React.useState(defaultRating);
   const [tempRating, setTempRating] = React.useState(0);
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color: color,
+    fontSize: `${size / 1.5}rem`,
+  };
+
+  const handleRating = (rating) => {
+    setRating(rating);
+    onSetRating(rating);
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={StarContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
-            onRate={() => setRating(i + 1)}
+            // onRate={() => setRating(i + 1)}
+            onRate={() => handleRating(i + 1)}
             onHoverIn={() => setTempRating(i + 1)}
             onHoverOut={() => setTempRating(0)}
             full={tempRating ? i < tempRating : i < rating}
+            color={color}
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating ? tempRating : rating > 0 && rating}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 };
 
-const starStyle = {
-  height: "3rem",
-  width: "3rem",
-  display: "block",
-  cursor: "pointer",
-};
+const Star = ({ onRate, full, onHoverIn, onHoverOut, color, size }) => {
+  const starStyle = {
+    width: `${size}rem`,
+    height: `${size}rem`,
+    display: "block",
+    cursor: "pointer",
+  };
 
-const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
   return (
     <span
       style={starStyle}
@@ -59,8 +81,8 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#000"
-          stroke="#000"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -69,7 +91,7 @@ const Star = ({ onRate, full, onHoverIn, onHoverOut }) => {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#000"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,31 @@
+import React from "react";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import StarRating from "./StarRating";
 
+const Test = () => {
+  const [movieRating, setMovieRating] = React.useState(0);
+  return (
+    <div>
+      <StarRating color="red" maxRating={10} onSetRating={setMovieRating} />
+      <h2>This movie was rated {movieRating} stars</h2>
+    </div>
+  );
+};
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
     <StarRating maxRating={10} />
+    <StarRating
+      maxRating={5}
+      size={2}
+      color="yellowgreen"
+      className="test"
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={4}
+    />
+    <Test />
   </StrictMode>,
 );
